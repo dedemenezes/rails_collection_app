43f56197c36c1e39077cc3ad13f8d1902e461b7b
Andre Menezes <djdedemenezes@gmail.com>
2021-09-01 19:54:23 -0300

    added default value to admin column

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_09_01_224538) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
