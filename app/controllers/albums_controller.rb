afc12b941cedf34cc807cbd76cb99ae0b8e337a6
Andre Menezes <djdedemenezes@gmail.com>
2021-09-01 22:07:31 -0300

    generated route and view new action

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,14 @@
+require 'open-uri'
 class AlbumsController < ApplicationController
 
   def index
     @albums = policy_scope(Album)
+  end
+
+  def new
+    @album = Album.new
+    @task = URI.open("https://moat.ai/api/task/", "Authorization" => "BasicZGV2ZWxvcGVyOlpHVjJaV3h2Y0dWeQ")
+    raise
+    @task
   end
 end
