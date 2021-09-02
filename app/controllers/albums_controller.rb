e2868c07ba750b5d0508e5824c536075b7ce0a2b
Andre Menezes <djdedemenezes@gmail.com>
2021-09-02 10:54:16 -0300

    can create an album using artist from the api

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,8 +5,27 @@ class AlbumsController < ApplicationController
     @albums = policy_scope(Album)
   end
 
+  
+
   def new
     @album = Album.new
     authorize @album
+  end
+
+  def create
+    @album = Album.new(album_params)
+    authorize @album
+    if @album.save
+      redirect_to albums_path
+      flash[:notice] = "Album created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:artist, :album_name, :year)
   end
 end
