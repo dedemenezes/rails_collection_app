976b1ea358f548b7e00158f4f5e10e2de6869442
Andre Menezes <djdedemenezes@gmail.com>
2021-09-02 14:18:39 -0300

    seeding two users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning DB"
+
+User.destroy_all
+Album.destroy_all
+
+puts "DB Clean\nzo/"
+
+puts "Creating users"
+User.create!(admin:true, username: 'admin', email:'admin@user.com', password: 123456, full_name:'Admin User')
+User.create!(username: 'basic', email: 'basic@user.com', password: 123456, full_name: 'Basic User')
+
+puts "#{User.count} users created"
