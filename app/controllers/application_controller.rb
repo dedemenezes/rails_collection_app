f53771729d8392fea267d54c887fdfb8b4c017d2
Andre Menezes <djdedemenezes@gmail.com>
2021-09-02 12:41:58 -0300

    user redirect to artists list page after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Rediect user after sign_in
+  def after_sign_in_path_for(resource)
+    artists_path
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :username])
