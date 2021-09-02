ac59cc64c72be416d23af12eef2211f49215022e
Andre Menezes <djdedemenezes@gmail.com>
2021-09-02 12:31:15 -0300

    accessing api artist info data

--- app/services/requests/artists.rb
+++ app/services/requests/artists.rb
@@ -10,5 +10,10 @@ module Requests
     def self.names
       list.map { |artist_item| artist_item.first['name'] }
     end
+
+    def self.artist_info(id)
+      response = Faraday.get("https://www.moat.ai/api/task/?artist_id=#{id}", {params: nil}, { "Basic" => "ZGV2ZWxvcGVyOlpHVjJaV3h2Y0dWeQ=="})
+      JSON.parse(response.body)
+    end
   end
 end
