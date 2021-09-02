026cab5347987b8db7a85d1b61eaa1d3645b398e
Andre Menezes <djdedemenezes@gmail.com>
2021-09-02 11:03:53 -0300

    show action and plicy generated

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,13 @@
 require 'faraday'
 class AlbumsController < ApplicationController
+  before_action :set_album, except: %i( new create)
 
   def index
     @albums = policy_scope(Album)
   end
 
-
+  def show
+  end
 
   def new
     @album = Album.new
@@ -25,6 +27,11 @@ class AlbumsController < ApplicationController
   end
 
   private
+
+  def set_album
+    @album = Album.find(params[:id])
+    authorize @album
+  end
 
   def album_params
     params.require(:album).permit(:artist, :album_name, :year)
--- app/policies/album_policy.rb
+++ app/policies/album_policy.rb
@@ -12,4 +12,8 @@ class AlbumPolicy < ApplicationPolicy
   def create?
     true
   end
+
+  def show?
+    true
+  end
 end
