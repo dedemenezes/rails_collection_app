71533eb9555bd42ba349ba16649f4c82516017ee
Andre Menezes <djdedemenezes@gmail.com>
2021-09-02 10:36:58 -0300

    added year validations to album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,5 @@
 class Album < ApplicationRecord
   validates :artist, :album_name, :year, presence:  true
+  validates :year, length: { is: 4 }
+  validates :year, numericality: { less_than_or_equal_to: Date.today.year }
 end
